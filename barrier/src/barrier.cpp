873734e6b7f1da64fd4c8580c3dbaaf12d4ff15d
gaetancollaud <gaetancollaud@gmail.com>
2017-05-25 20:23:58 +0200

    set node id

--- barrier/src/barrier.cpp
+++ barrier/src/barrier.cpp
@@ -1,10 +1,22 @@
+#define MY_NODE_ID 20
+#define MY_DEBUG
+#define MY_RADIO_NRF24
+#define PIN_LED LED_BUILTIN
+#define BARRIER_SENSOR_ID 10
+
 #include <Arduino.h>
 #include <light-barrier.h>
-
-#define PIN_LED LED_BUILTIN
+#include <MySensors.h>
 
 LightBarrier lightBarrier;
 
+
+MyMessage msgBarrier(BARRIER_SENSOR_ID, V_STATUS);
+
+void presentation(){
+	sendSketchInfo("LightBarrier", "1.0");
+  present(BARRIER_SENSOR_ID, S_BINARY);
+}
 
 void setup(){
   Serial.begin(115200);
@@ -13,9 +25,22 @@ void setup(){
   lightBarrier.init();
 }
 
+bool lastObstacle = false;
+void sendMessage(bool obstacle){
+  if(lastObstacle!=obstacle){
+    lastObstacle = obstacle;
+    send(msgBarrier.set(obstacle));
+  }
+}
+
+
 void loop(){
   unsigned long now = millis();
 
   lightBarrier.loop(now);
-  digitalWrite(PIN_LED, lightBarrier.isObstacle() ? HIGH : LOW);
+
+
+  bool obstacle = lightBarrier.isObstacle();
+  // digitalWrite(PIN_LED, obstacle ? HIGH : LOW);
+  sendMessage(obstacle);
 }
